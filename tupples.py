64b99fa02cb73fd721ec522ee2dafecdcb20891a
nageshkumar13 <nageshkumar1925@gmail.com>
2025-04-01 12:58:44 +0530

    tupples

---
+++ tupples.py
@@ -0,0 +1,11 @@
+coordinates = (10, 20, 30)
+print(f"Coordinates tuple: {coordinates}")
+
+# Indexing
+print(f"First coordinate: {coordinates[0]}")  # 10
+print(f"Second coordinate: {coordinates[1]}") # 20
+
+# Immutability
+# coordinates[0] = 100  # This will cause an error!  Tuples cannot be modified.
+
+
